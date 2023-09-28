f58877ea9bd856ce5829ef86cf0f9f05b127fe6a
Akshayknchalapalli <akshaysmart31@gmail.com>
2023-09-28 20:15:09 +0530

    commit chnages

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,54 +1,29 @@
 import React, { useState } from 'react';
 import Draggable from 'react-draggable'; 
-import { Resizable } from 'react-resizable';
+import Resizable from 'react-resizable';
 
-const Overlay = ({ src, initialPosition, initialSize }) => {
-
-  // Manage position and size in local state
-  const [position, setPosition] = useState(initialPosition);
-  const [size, setSize] = useState(initialSize);
-
-  const handleResize = (e, { size }) => {
-    // Update size state on resize
-    setSize({
-      width: size.width,
-      height: size.height
-    });
-  };
-  
-  const handleDrag = (e, ui) => {
-    // Update position state on drag
-    setPosition({
-      x: ui.x, 
-      y: ui.y
-    });
-  };
+function Overlay({overlay}) {
 
   return (
-    <Draggable 
-      onDrag={handleDrag}
-      position={position}
-    >
-      <Resizable
-        width={size.width}
-        height={size.height}
-        onResize={handleResize}
-      >
-        <div
-          style={{
-            position: 'absolute', 
-            left: position.x,
-            top: position.y,
-            width: size.width,
-            height: size.height,
-            border: '1px solid #000' 
-          }}
-        >
-          <video src={src} style={{ width: '100%', height: '100%' }} />
-        </div>
+    <Draggable>
+      <Resizable width={overlay.width} height={overlay.height}>
+        <div>{overlay.name}</div>
       </Resizable>
     </Draggable>
-  );
-};
+  )
 
-export default Overlay;
+}
+
+function OverlayManager() {
+
+  const [overlays, setOverlays] = useState([/* array of overlays */]);
+
+  return (
+    <div>
+     {overlays.map(overlay => (
+       <Overlay key={overlay.id} overlay={overlay} />  
+     ))}
+    </div>
+  )
+
+}
--- src/components/OverlayManager/OverlayManager.js
+++ src/components/OverlayManager/OverlayManager.js
@@ -1,78 +1,150 @@
-import React, { useState } from 'react';
-import Video from './Video';
-import Overlay from './Overlay';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+import styles from './OverlayManager.module.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const OverlayManager = () => {
+function OverlayManager() {
+
   const [overlays, setOverlays] = useState([]);
-  const [newOverlay, setNewOverlay] = useState({
-    src: '', 
-    position: 'absolute',
-    size: { width: '100px', height: '100px' }
-  });
 
-  const addOverlay = () => {
-    setOverlays([...overlays, {...newOverlay, id: Date.now()}]);
-    setNewOverlay({
-      src: '',
-      position: 'absolute',
-      size: { width: '100px', height: '100px' }
-    });
-  };
+  useEffect(() => {
+    getOverlays();
+  }, []);
 
-  const removeOverlay = (id) => {
-    setOverlays(overlays.filter(overlay => overlay.id !== id));
-  };
+  const getOverlays = async () => {
+    try {
+      const response = await axios.get('/api/overlays');
+      setOverlays(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  const updateOverlay = (id, updatedOverlay) => {
-    const updatedOverlays = overlays.map(overlay => {
-      if (overlay.id === id) {
-        return updatedOverlay;  
-      }
-      return overlay;
-    });
-    setOverlays(updatedOverlays);
-  };
+  const addOverlay = async (overlay) => {
+    try {
+      const response = await axios.post('/api/overlays', overlay);
+      setOverlays([...overlays, response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const updateOverlay = async (updatedOverlay) => {
+    try {
+      await axios.put(`/api/overlays/${updatedOverlay.id}`, updatedOverlay);
+      setOverlays(overlays.map(o => {
+        if (o.id === updatedOverlay.id) {
+          return updatedOverlay;
+        }
+        return o;
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const deleteOverlay = async (overlayId) => {
+    try {
+      await axios.delete(`/api/overlays/${overlayId}`);
+      setOverlays(overlays.filter(o => o.id !== overlayId));
+    } catch (error) {
+      console.log(error);  
+    }
+  }
 
   return (
-    <>
-      <Video src="/livestream.mp4" />
-      
-      <div>
-        <h2>Overlay Management</h2>
-        
-        {/* Overlay Form */}
-        <h3>Add Overlay</h3>
-        <form onSubmit={addOverlay}>
-          {/* Form fields to update newOverlay state */}
-          <button type="submit">Add</button> 
-        </form>
-        
-        {/* Display Existing Overlays */}
-        <h3>Existing Overlays</h3>
-        
-        <ul>
-          {overlays.map(overlay => (
-            <li key={overlay.id}>
-              <Overlay {...overlay} />
-              
-              <button onClick={() => removeOverlay(overlay.id)}>
-                Remove
-              </button>
-              
-              <button 
-                onClick={() => updateOverlay(overlay.id, {
-                  ...overlay,  
-                  size: { width: '200px', height: '200px' } 
-                })}>
-                Update Size
-              </button>
-            </li>
-          ))}
-        </ul>
-        
-      </div>
-    </>
+    <div>
+      <h2>Manage Overlays</h2>
+
+      <button onClick={() => addOverlay({id: 3, name: 'New Overlay'})}>
+        Add Overlay
+      </button>
+
+      {overlays.map(overlay => (
+        <div key={overlay.id}>
+          {overlay.name}
+          
+          <button onClick={() => updateOverlay({...overlay, name: 'Updated Name'})}>
+            Update
+          </button>
+
+          <button onClick={() => deleteOverlay(overlay.id)}>
+            Delete
+          </button>
+        </div>
+      ))}
+
+    </div>
   );
 }
+// Styling with CSS Modules
 
-export default OverlayManager;
+export default function OverlayManager() {
+
+   const [overlays, setOverlays] = useState([
+     {
+       id: 1,
+       text: 'Hello World',
+       color: 'red'  
+     },
+     {
+       id: 2, 
+       text: 'My Overlay',
+       color: 'blue'
+     }
+   ]);
+ 
+   return (
+     <div className={styles.container}>
+       <h1>Overlay Manager</h1>
+ 
+       <OverlaysList overlays={overlays} />
+ 
+       <OverlayPreview overlay={overlays[0]} />
+ 
+       <AddOverlay 
+         onAdd={(text, color) => {
+           setOverlays(prev => [...prev, {id: nextId, text, color}]);
+         }}
+       />
+     </div>
+   );
+ }
+ 
+ // Reusable OverlaysList component
+ function OverlaysList({overlays}) {
+   return (
+     <ul>
+       {overlays.map(overlay => (
+         <li key={overlay.id}>{overlay.text}</li>  
+       ))}
+     </ul>
+   );
+ }
+ 
+ // Show preview of selected overlay 
+ function OverlayPreview({overlay}) {
+   return (
+     <div style={{color: overlay.color}}>
+       {overlay.text}
+     </div>
+   );
+ }
+ 
+ // Form to add new overlays
+ function AddOverlay({onAdd}) {
+ 
+   const [text, setText] = useState('');
+   const [color, setColor] = useState('red');
+ 
+   const handleSubmit = e => {
+     e.preventDefault();
+     onAdd(text, color);
+     setText('');
+   }
+ 
+   // ... form JSX
+ 
+   return <form onSubmit={handleSubmit}>...</form>;
+ }
