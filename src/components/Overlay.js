79e5ab8107639f375b8d3bf2ae59e1ce003176af
Akshayknchalapalli <akshaysmart31@gmail.com>
2023-09-28 19:26:10 +0530

    installed a package and updated Overlay.js

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Draggable from 'react-draggable'; 
+import { Resizable } from 'react-resizable';
 
-const Overlay = ({ src, position, size }) => {  
+const Overlay = ({ src, initialPosition, initialSize }) => {
+
+  // Manage position and size in local state
+  const [position, setPosition] = useState(initialPosition);
+  const [size, setSize] = useState(initialSize);
+
+  const handleResize = (e, { size }) => {
+    // Update size state on resize
+    setSize({
+      width: size.width,
+      height: size.height
+    });
+  };
+  
+  const handleDrag = (e, ui) => {
+    // Update position state on drag
+    setPosition({
+      x: ui.x, 
+      y: ui.y
+    });
+  };
+
   return (
-    <video 
-      style={{
-        position,
-        width: size.width,
-        height: size.height
-      }}  
-      src={src}
-    />
+    <Draggable 
+      onDrag={handleDrag}
+      position={position}
+    >
+      <Resizable
+        width={size.width}
+        height={size.height}
+        onResize={handleResize}
+      >
+        <div
+          style={{
+            position: 'absolute', 
+            left: position.x,
+            top: position.y,
+            width: size.width,
+            height: size.height,
+            border: '1px solid #000' 
+          }}
+        >
+          <video src={src} style={{ width: '100%', height: '100%' }} />
+        </div>
+      </Resizable>
+    </Draggable>
   );
-}
+};
 
-export default Overlay;  // Keep this export statement, remove the previous one
+export default Overlay;
