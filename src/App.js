f048a21f6c047d0209fd569f250c86007e762933
Akshayknchalapalli <akshaysmart31@gmail.com>
2023-09-29 14:37:37 +0530

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 // import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
