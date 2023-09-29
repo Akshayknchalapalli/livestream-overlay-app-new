23a2349a2b2f200a6d26044485671053e0935eb5
Akshayknchalapalli <akshaysmart31@gmail.com>
2023-09-29 21:38:03 +0530

    integrating frontend

--- src/App.js
+++ src/App.js
@@ -10,6 +10,27 @@ function App() {
   const [overlays, setOverlays] = useState([]);
   const rtspUrl = 'http://localhost:5000/livestream/rtmp://7ef85eea2114.entrypoint.cloud.wowza.com/app-8Mr993br'; // Replace with your RTSP URL
 
+  // Create a JSON object with the RTSP URL
+  const data = { rtsp_url: rtspUrl };
+
+  // Make a POST request to the Flask route
+  fetch('/rtsp-url', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+      // Handle the response from the backend, if needed
+      console.log(data.message);
+  })
+  .catch(error => {
+      // Handle any errors that occur during the POST request
+      console.error('Error:', error);
+  });
+
   // Inside your component's render method
   return (
     <div className="container">
