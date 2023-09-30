e24071bd32718faa8ca719fb1d7205b000ea53e0
Akshayknchalapalli <akshaysmart31@gmail.com>
2023-09-30 14:35:03 +0530

    updated Overlay.js file

--- src/components/OverlayManager/Overlay.js
+++ src/components/OverlayManager/Overlay.js
@@ -1,50 +1,20 @@
-import React, { useState } from 'react';
-import Draggable from 'react-draggable';
-import { Resizable } from 'react-resizable';
-import './Overlay.css';
+function Overlay({ position, size, content }) {
 
-function Overlay({ position, size, content, onUpdate }) {
-  // State variables for position and size
-  const [overlayPosition, setOverlayPosition] = useState(position);
-  const [overlaySize, setOverlaySize] = useState(size);
-
-  // Function to handle position change
-  const handleDrag = (e, ui) => {
-    const { x, y } = ui.deltaPosition;
-    const newPosition = { x: overlayPosition.x + x, y: overlayPosition.y + y };
-    setOverlayPosition(newPosition);
-    onUpdate(newPosition, overlaySize);
-  };
-
-  // Function to handle size change
-  const handleResize = (e, { size }) => {
-    setOverlaySize(size);
-    onUpdate(overlayPosition, size);
-  };
-
-  // Style the overlay based on position and size
+  // Style based on props 
   const overlayStyle = {
     position: 'absolute',
-    left: overlayPosition.x,
-    top: overlayPosition.y,
-    width: overlaySize.width,
-    height: overlaySize.height,
-    border: '1px solid #ccc',
-    // Add any other styles you want here
+    left: position.x,
+    top: position.y,
+    width: size.width,
+    height: size.height
   };
 
   return (
-    <Draggable onDrag={handleDrag}>
-      <Resizable width={size.width} height={size.height} onResize={handleResize}>
-        <div className="overlay-container" style={overlayStyle}>
-          <div className="overlay-content">
-            {/* Render overlay content */}
-            {content}
-          </div>
-        </div>
-      </Resizable>
-    </Draggable>
+    <div className="overlay" style={overlayStyle}>
+      <div className="overlay-content">
+        {content}
+      </div>
+    </div>
   );
-}
 
-export default Overlay;
+}
